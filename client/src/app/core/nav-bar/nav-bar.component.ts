a98da972ec93eab93cf73c5fba3fb0c92e47341d
Yanxiao Fang <axiaobug@hotmail.com>
2021-07-16 14:34:35 +1000

    feat(core,interceptor,nav-bar):
    Add Error handling with interceptor

--- client/src/app/core/nav-bar/nav-bar.component.ts
+++ client/src/app/core/nav-bar/nav-bar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+
 
 }
