3e46e3627697522d09ec619c9fff7d3332ad4ea2
Yanxiao Fang <axiaobug@hotmail.com>
2021-08-01 13:03:58 +1000

    fix bug

--- API/Program.cs
+++ API/Program.cs
@@ -16,32 +16,32 @@ namespace API
     {
         public static async Task Main(string[] args)
         {
-            //var host = CreateHostBuilder(args).Build();
-            //using (var scope = host.Services.CreateScope())
-            //{
-            //    var services = scope.ServiceProvider;
-            //    var loggerFactory = services.GetRequiredService<ILoggerFactory>();
-            //    try
-            //    {
-            //        var context = services.GetRequiredService<StoreContext>();
-            //        //await context.Database.MigrateAsync();
-            //        //await StoreContextSeed.SeedAsync(context, loggerFactory);
+            //CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+                try
+                {
+                    var context = services.GetRequiredService<StoreContext>();
+                    await context.Database.MigrateAsync();
+                    await StoreContextSeed.SeedAsync(context, loggerFactory);
 
 
-            //        var userManager = services.GetRequiredService<UserManager<AppUser>>();
-            //        var identityContext = services.GetRequiredService<AppIdentityDbContext>();
-            //        await identityContext.Database.MigrateAsync();
-            //        await AppIdentityDbContextSeed.SeedUsersAsync(userManager);
-            //    }
-            //    catch (Exception e)
-            //    {
-            //        var logger = loggerFactory.CreateLogger<Program>();
-            //        logger.LogError(e, "An error occurred during migration ");
-            //        throw;
-            //    }
-            await CreateHostBuilder(args).Build().RunAsync();
-            
-            //await host.RunAsync();
+                    var userManager = services.GetRequiredService<UserManager<AppUser>>();
+                    var identityContext = services.GetRequiredService<AppIdentityDbContext>();
+                    await identityContext.Database.MigrateAsync();
+                    await AppIdentityDbContextSeed.SeedUsersAsync(userManager);
+                }
+                catch (Exception e)
+                {
+                    var logger = loggerFactory.CreateLogger<Program>();
+                    logger.LogError(e, "An error occurred during migration ");
+                    throw;
+                }
+            }
+            await host.RunAsync();
 
         }
 
